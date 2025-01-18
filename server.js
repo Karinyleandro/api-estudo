a0fdb28984ed52505f7d728fd4ce50d2e0bb9cc6
Kariny <karinysilva.estudo@gmail.com>
2025-01-17 22:07:51 -0300

    aprendendo sobre rotas

--- server.js
+++ server.js
@@ -2,3 +2,22 @@ import express from 'express';
 
 const app = express();
 
+/*
+1) tipo de rota
+2) url (endereço)
+
+
+HTTP MÉTODOS:
+ get: listar
+ post: criar;
+ put: editar;
+ patch: editar um;
+ delete: deletar;
+*/
+
+// req, res => requisição e resposta
+app.get('/usuarios', (req, res) =>{
+    res.send('Ok, deu bom');
+});
+
+app.listen(3000);
