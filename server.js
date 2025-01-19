cd9315e28f86866baaf5ff51b0911d43c359947c
Kariny <karinysilva.estudo@gmail.com>
2025-01-19 15:41:01 -0300

    Verificando servidor e testando

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
-import express from 'express';
-
-const app = express();
+// No server.js a gente vai ter as principais rotas dos nossos códigos
 
 /*
 1) tipo de rota
 2) url (endereço)
-
-
 HTTP MÉTODOS:
  get: listar
  post: criar;
@@ -15,9 +11,40 @@ HTTP MÉTODOS:
  delete: deletar;
 */
 
-// req, res => requisição e resposta
-app.get('/usuarios', (req, res) =>{
-    res.send('Ok, deu bom');
+/*
+Query Params
+Route Params: get, put, delete
+Body Params: post e put
+*/
+
+//importando a biblioteca do express
+import express from 'express';
+// criando uma variável e passando o express como uma função
+const app = express();
+app.use(express.json());
+
+const users = []
+
+app.post('/usuarios' , (req, res) =>{
+    users.push(req.body);
+    res.status(201).json(req.body);
 });
 
+// req, res => requisição e resposta
+app.get('/usuarios', (req, res) =>{
+    res.json(users)
+    res.status(200).json(users)
+});
+
+
 app.listen(3000);
+
+
+/* 
+    Criar API de usuarios
+
+    - criar um usuario
+    - listar todos os usuarios
+    - editar um usuario
+    - deletar um usuario
+*/
